affac3e9ebd40ce14de83051302742271f2b8b35
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-06-29 22:52:45 +0300

    add roles

--- LMS.api/Controllers/CourseController.cs
+++ LMS.api/Controllers/CourseController.cs
@@ -1,5 +1,7 @@
 ﻿using LMS.Application.DTOs;
 using LMS.Application.Interfaces;
+using LMS.Data.Consts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LMS.Api.Controllers
@@ -10,6 +12,7 @@ namespace LMS.Api.Controllers
     {
         private readonly ICourseService _courseService = courseService;
 
+        [Authorize(Roles = $"${ConstRoles.Teacher}")]
         [HttpPost]
         public async Task<IActionResult> CreateCourse(CourseDTO courseDto)
         {
@@ -19,7 +22,7 @@ namespace LMS.Api.Controllers
             var result = await _courseService.CreateCourse(courseDto);
             return result ? Ok("created Sucsessfully") : BadRequest(ModelState);
         }
-
+        [Authorize(Roles = $"${ConstRoles.Teacher}")]
         [HttpPut]
         public async Task<IActionResult> UpdateCourse(string id, CourseDTO courseDto)
         {
@@ -30,6 +33,7 @@ namespace LMS.Api.Controllers
             return result ? Ok("updated Sucsessfully") : BadRequest("failed to update");
 
         }
+        [Authorize(Roles = $"${ConstRoles.Teacher}")]
 
         [HttpDelete]
         public async Task<IActionResult> DeleteCourse(string id)
@@ -38,7 +42,7 @@ namespace LMS.Api.Controllers
             return result ? Ok("deleted Sucsessfully") : BadRequest("failed to delete");
 
         }
-
+        [Authorize(Roles = $"${ConstRoles.Admin}")]
         [HttpPost("enroll")]
         public async Task<IActionResult> EnrollStudentInCourse(string studentEmail,string courseCode)
         {
@@ -46,42 +50,42 @@ namespace LMS.Api.Controllers
             return result ? Ok("student has been added Sucsessfully") : BadRequest("failed to add student");
 
         }
-
+        [Authorize]
         [HttpGet("all")]
         public async Task<IActionResult> GetAllCourses()
         {
             var courses = await _courseService.GetAllCourses();
             return Ok(courses);
         }
-
+        [Authorize]
         [HttpGet("search")]
         public async Task<IActionResult> GetCoursesByCrateria(string carteria)
         {
             var courses = await _courseService.SearchForCources(carteria);
             return Ok(courses);
         }
-
+        [Authorize]
         [HttpGet("by-id")]
         public async Task<IActionResult> GetCourse(string id)
         {
             var course = await _courseService.GetCourse(id);
             return course != null ?  Ok(course) : throw new Exception("course not found") ;
         }
-
+        [Authorize]
         [HttpGet("teacher-courses")]
         public async Task<IActionResult> GetCoursesByTeacherId(string teacherId)
         {
             var courses = await _courseService.GetCoursesByTeacherId(teacherId);
             return Ok(courses);
         }
-
+        [Authorize]
         [HttpGet("count")]
         public async Task<IActionResult> GetNumberOfCourses()
         {
             var count = await _courseService.GetNumberOfCourses();
             return Ok(count);
         }
-
+        [Authorize]
         [HttpGet("students-count-in-course")]
         public async Task<IActionResult> GetStudentCountInCourse(string courseId)
         {
