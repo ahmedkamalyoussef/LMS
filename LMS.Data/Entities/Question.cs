839645b66358c0f5e14a3f47550b6542e9715f8a
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-06-30 14:02:57 +0300

    edit question in database

--- LMS.Data/Entities/Question.cs
+++ LMS.Data/Entities/Question.cs
@@ -5,6 +5,7 @@ namespace LMS.Data.Entities
     public class Question
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Content { get; set; }
         public string ExamId { get; set; }
         public Exam Exam { get; set; }
         public ICollection<Answer>? Answers { get; set; } = new HashSet<Answer>();
